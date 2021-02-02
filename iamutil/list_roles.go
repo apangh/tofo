521e0fbb137d73ad3107ac2e1545b9b825690a10
Anthony Pang <selfie2021work@gmail.com>
2021-02-01 19:16:29 -0800

    List roles

---
+++ iamutil/list_roles.go
@@ -0,0 +1,32 @@
+package iamutil
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
+	"github.com/aws/aws-sdk-go-v2/service/iam/types"
+)
+
+type ListRolesCB interface {
+	Do(ctx context.Context, role types.Role) error
+}
+
+func ListRoles(ctx context.Context, client *iam.Client, cb ListRolesCB) error {
+	params := &iam.ListRolesInput{
+		MaxItems: aws.Int32(100),
+	}
+	p := iam.NewListRolesPaginator(client, params)
+	for p.HasMorePages() {
+		o, err := p.NextPage(ctx)
+		if err != nil {
+			return err
+		}
+		for _, role := range o.Roles {
+			if e := cb.Do(ctx, role); e != nil {
+				return e
+			}
+		}
+	}
+	return nil
+}
