ee0861fa0f95f249b294512f4f3ae3da9f527267
apangh <78009936+apangh@users.noreply.github.com>
2021-02-01 18:50:16 -0800

    ListUsers (#12)

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.1.0
 	github.com/aws/aws-sdk-go-v2/config v1.0.0
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.1.0
+	github.com/aws/aws-sdk-go-v2/service/iam v1.1.0
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.0.0
 	github.com/aws/aws-sdk-go-v2/service/sqs v1.1.0
 	github.com/aws/smithy-go v1.0.0
---
+++ iamutil/list_users/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	"github.com/apangh/tofo"
+	"github.com/apangh/tofo/iamutil"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
+	"github.com/golang/glog"
+)
+
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	logToStderr := flag.Lookup("alsologtostderr")
+	if err := logToStderr.Value.Set("true"); err != nil {
+		fmt.Printf("Failed to setup glog: %v", err)
+	}
+
+	ctx := context.Background()
+	config, err := config.LoadDefaultConfig(ctx,
+		config.WithSharedConfigProfile("administrator"))
+	if err != nil {
+		glog.Errorf("Failed to list buckets: %s\n", err)
+		return
+	}
+	client := iam.NewFromConfig(config)
+
+	l := &iamutil.LogUser{}
+
+	e := iamutil.ListUsers(ctx, client, l)
+	if e != nil {
+		tofo.LogErr("ListUsers", e)
+		glog.Errorf("Failed to list users: %v", e)
+		return
+	}
+
+}
