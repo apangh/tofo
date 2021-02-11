38191ddbdfebafb3d2b40168bdd696af6b3054c8
apangh <78009936+apangh@users.noreply.github.com>
2021-02-11 14:51:03 -0800

    GetTrailStatus (#39)

--- cloudtrailutil/walk.go
+++ cloudtrailutil/walk.go
@@ -2,14 +2,35 @@ package cloudtrailutil
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/apangh/tofo/model"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cloudtrail"
 )
+
+type TrailStatusCB struct {
+	client *cloudtrail.Client
+}
+
+func (ts *TrailStatusCB) Do(ctx context.Context, t *model.Trail) error {
+	params := cloudtrail.GetTrailStatusInput{
+		Name: aws.String(t.Name),
+	}
+	o, e := ts.client.GetTrailStatus(ctx, &params)
+	if e != nil {
+		return e
+	}
+	fmt.Printf("TrailStatus[%s]: %+v\n", t.Name, o)
+	return nil
+}
 
 func Walk(ctx context.Context, client *cloudtrail.Client, orm *model.ORM) error {
 	if e := DescribeTrails(ctx, client,
 		&TrailRecorder{orm: orm}); e != nil {
+		return e
+	}
+	if e := orm.TrailModel.Scan(ctx, &TrailStatusCB{client: client}); e != nil {
 		return e
 	}
 	return nil
