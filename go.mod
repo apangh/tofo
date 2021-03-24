c7c982a8779f45b85859e175b4b38b1cc58bc173
apangh <78009936+apangh@users.noreply.github.com>
2021-03-23 19:52:14 -0700

    s3 writer (move from ramen to tofo) (#51)

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/apangh/tofo
 go 1.15
 
 require (
-	github.com/apangh/salt v0.0.0-20210321024032-7c512abafc4e
-	github.com/apangh/tofo/logger v0.0.0-20210320204253-610bb3b5dc4f
+	github.com/apangh/salt v0.0.0-20210324024303-8daecc2421b7
+	github.com/apangh/tofo/logger v0.0.0-20210321045018-1d370ccc8ade
 	github.com/aws/aws-sdk-go-v2 v1.3.0
 	github.com/aws/aws-sdk-go-v2/config v1.1.3
 	github.com/aws/aws-sdk-go-v2/service/cloudtrail v1.2.0
---
+++ writer/s3/writer.go
@@ -0,0 +1,76 @@
+// This file implements writer for S3 objects
+package s3writer
+
+import (
+	"context"
+	"net/url"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/middleware"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+
+	"github.com/apangh/salt/logger"
+	"github.com/apangh/salt/memory/mpool"
+	"github.com/apangh/salt/writer"
+	awslogger "github.com/apangh/tofo/logger"
+)
+
+// s3 writer
+type s3Writer struct {
+	client *s3.Client
+	bucket string
+}
+
+var _ writer.Writer = (*s3Writer)(nil)
+
+// NewWriter creates a s3 writer
+func NewWriter(client *s3.Client, bucket string) writer.Writer {
+	return &s3Writer{
+		client: client,
+		bucket: bucket,
+	}
+}
+
+// do is an internal implementation of Do
+func (w *s3Writer) do(ctx context.Context, i writer.Input) (writer.Output, error) {
+	start := time.Now()
+	s3tags := url.Values{}
+	for k, v := range i.GetTags() {
+		s3tags.Add(k, v)
+	}
+	params := s3.PutObjectInput{
+		Bucket:   aws.String(w.bucket),
+		Key:      aws.String(i.GetKey()),
+		Body:     mpool.NewBufsReadSeeker(i.GetBufs()),
+		Metadata: i.GetMetadata(),
+		Tagging:  aws.String(s3tags.Encode()),
+	}
+	o, e := w.client.PutObject(ctx, &params)
+	elapsed := time.Since(start)
+	if e != nil {
+		awslogger.LogAPIError(ctx, e, "Put object %s/%s", w.bucket, i.GetKey())
+		logger.Errorf(ctx, "Put object %s/%s failed - elapsed: %+v", w.bucket, i.GetKey(),
+			elapsed)
+		return nil, e
+	}
+	output, e := i.MakeOutput(ctx, elapsed)
+	if e != nil {
+		return nil, e
+	}
+	requestId, _ := middleware.GetRequestIDMetadata(o.ResultMetadata)
+	logger.Infof(ctx, "Put object %s/%s, requestID: %s, elapsed: %+v, throughput: %.2f B/s",
+		w.bucket, i.GetKey(), requestId, elapsed,
+		float64(i.GetBufs().GetSize())/elapsed.Seconds())
+	return output, nil
+}
+
+// Do creates a S3 object with content from bufs
+func (w *s3Writer) Do(ctx context.Context, i writer.Input) (writer.Output, error) {
+	o, e := w.do(ctx, i)
+	if e != nil {
+		i.RegisterError(ctx, e)
+		return nil, e
+	}
+	return o, nil
+}
